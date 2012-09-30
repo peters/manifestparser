0f1d650bd311064eb04cace0a88a00b9dbbd2c6d
Peter Sunde <peter.sunde@gmail.com>
2012-09-30 03:53:31 +0200

    Added more fixtures

--- tests/plist.test.js
+++ tests/plist.test.js
@@ -2,7 +2,7 @@ var PlistReader = require('../lib/plistreader');
 	should = require('should'),
 	fs = require('fs'),
 	fixturesDir = __dirname + '/fixtures';
-describe("Plist (plist) Reader", function() {
+describe("Plist Reader", function() {
 	it('should parse binary plist and return it as xml', function(done) {
 		var plistBinary = fixturesDir + '/binary.plist',
 			plistLocal = fs.readFileSync(fixturesDir + '/plain.plist').toString('utf-8');
@@ -10,6 +10,18 @@ describe("Plist (plist) Reader", function() {
 			outputFormat: 'xml'
 		}).on('plist', function(plist) {
 			plist.should.equal(plistLocal);
+		 	done();
+		}).on('error', function(err) {
+			process.stdout.write(err);
+		}).parse();
+	});
+	it('should parse binary plist and return it as json', function(done) {
+		var plistBinary = fixturesDir + '/binary.plist',
+			plistJSON = fs.readFileSync(fixturesDir + '/binary.plist.json').toString('utf-8');
+		new PlistReader(plistBinary, {
+			outputFormat: 'json'
+		}).on('plist', function(plist) {
+			plist.should.equal(plistJSON);
 		 	done();
 		}).on('error', function(err) {
 			process.stdout.write(err);
@@ -27,32 +39,36 @@ describe("Plist (plist) Reader", function() {
 			process.stdout.write(err);
 		}).parse();
 	});
-	it('should throw error on invalid filename (.plist)', function(done) {
-		new PlistReader(fixturesDir + '/non-existant-file.plist', {
-			outputFormat: 'xml'
+	it('should parse plist xml and return it as json', function(done) {
+		var plistPlain = fixturesDir + '/plain.plist',
+			plistJSON = fs.readFileSync(fixturesDir + '/plain.plist.json').toString('utf-8');
+		new PlistReader(plistPlain, {
+			outputFormat: 'json'
 		}).on('plist', function(plist) {
-			throw new Error("Should not happen");
+			plist.should.equal(plistJSON);
+		 	done();
 		}).on('error', function(err) {
-			err.should.be.an.instanceOf(Error);
-			done();
+			process.stdout.write(err);
 		}).parse();
 	});
-	it('should throw error on invalid filename (.zip)', function(done) {
-		new PlistReader(fixturesDir + '/non-existant-file.zip', {
-			outputFormat: 'xml'
-		}).on('plist', function(plist) {
-			throw new Error("Should not happen");
-		}).on('error', function(err) {
-			err.should.be.an.instanceOf(Error);
-			done();
-		}).parse();
+	it('should throw error on invalid filename (.test)', function(done) {
+		(function() {
+			new PlistReader(fixturesDir + '/non-existant-file.test', {
+				outputFormat: 'xml'
+			}).on('plist', function(plist) {
+				throw new Error("Should not happen");
+			}).on('error', function(err) {
+				throw new Error("Should not happen");
+			}).parse();
+		}).should.throw();
+		done();
 	});
 	it('should throw error if invalid output format', function(done) {
 		(function() {
 			var plistBinary = fixturesDir + '/plain.plist';
 			new PlistReader(plistBinary, {
 				outputFormat: 'non-existant-format'
-			}).on('manifest', function(plist) {
+			}).on('plist', function(plist) {
 				throw new Error("Should not have happend.");
 			}).on('error', function(err) {
 				throw new Error("Should not have happend.");
@@ -65,12 +81,88 @@ describe("Plist (plist) Reader", function() {
 			var plistBinary = fixturesDir + '/plain.plist-invalid';
 			new PlistReader(plistBinary, {
 				outputFormat: 'non-existant-format'
-			}).on('manifest', function(plist) {
+			}).on('plist', function(plist) {
 				throw new Error("Should not have happend.");
 			}).on('error', function(err) {
 				throw new Error("Should not have happend.");
 			}).parse();			
 		}).should.throw();
 		done();
+	});
+	it('should work on zip files and output as xml', function(done) {
+		var plistBinary = fixturesDir + '/Plists.zip',
+			plistXml = fs.readFileSync(fixturesDir + '/plain.plist').toString('utf-8'),
+			asserts = 0;
+		new PlistReader(plistBinary, {
+			outputFormat: 'xml'
+		}).on('plist', function(plist, filename) {
+			plist.should.equal(plistXml);
+			if(filename === 'plain.plist' || filename === 'binary.plist') {
+				asserts++;		
+			}
+		}).on('end', function(files) {	
+			(asserts).should.equal(2);
+			files.should.be.an.instanceOf(Array);
+			files.should.length(2);
+			files[0].content.should.equal(plistXml);
+			files[1].content.should.equal(plistXml);
+			done();
+		}).on('error', function(err) {
+			throw new Error("Should not have happend.");
+		}).parse();			
+	});
+	it('should work on zip files and output as json', function(done) {
+		var plistBinary = fixturesDir + '/Plists.zip',
+			plistJSON = fs.readFileSync(fixturesDir + '/plain.plist.json').toString('utf-8'),
+			asserts = 0;
+		new PlistReader(plistBinary, {
+			outputFormat: 'json'
+		}).on('plist', function(plist, filename) {
+			plist.should.equal(plistJSON);
+			if(filename === 'plain.plist' || filename === 'binary.plist') {
+				asserts++;		
+			}
+		}).on('end', function(files) {	
+			(asserts).should.equal(2);
+			files.should.be.an.instanceOf(Array);
+			files.should.length(2);
+			files[0].content.should.equal(plistJSON);
+			files[1].content.should.equal(plistJSON);
+			done();
+		}).on('error', function(err) {
+			throw new Error("Should not have happend.");
+		}).parse();			
+	});
+	it('it should work on ipa (ios files) and output xml', function(done) {
+		var ipaBinary = fixturesDir + '/Snake.ipa',
+			plists = 0;
+		new PlistReader(ipaBinary, {
+			outputFormat: 'xml'
+		}).on('plist', function(plist) {
+			plists++;
+		}).on('end', function(files) {
+			(plists).should.equal(9);
+			files.should.length(9);
+			done();
+		}).on('error', function(err) {
+			throw new Error("Should not have happend.");
+		}).parse();
+	});
+	it('it should work on ipa (ios files) and output json', function(done) {
+		var ipaBinary = fixturesDir + '/Snake.ipa',
+			plists = 0;
+		new PlistReader(ipaBinary, {
+			outputFormat: 'json'
+		}).on('plist', function(plist) {
+			var str = JSON.stringify(plist);
+			JSON.parse(str).should.equal(plist);
+			plists++;
+		}).on('end', function(files) {
+			(plists).should.equal(9);
+			files.should.length(9);
+			done();
+		}).on('error', function(err) {
+			throw new Error("Should not have happend.");
+		}).parse();
 	});
 });
